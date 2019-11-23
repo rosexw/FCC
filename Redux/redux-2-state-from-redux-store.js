3746bbe3d03061a5006099369abf9974c796a9b1
rosexw <rose.xw.wan@gmail.com>
2019-11-22 19:34:05 -0500

    First five exercises of Redux

--- Redux/redux-2-state-from-redux-store.js
+++ Redux/redux-2-state-from-redux-store.js
@@ -5,7 +5,7 @@ const store = Redux.createStore(
   // change code below this line
   const currentState = store.getState()
 
-// Redux: Get State from the Redux Store
+// Redux: Get State from the Redux Store 2/17
 // The Redux store object provides several methods that allow you to interact with it. For example, you can retrieve the current state held in the Redux store object with the getState() method.
 
 // The code from the previous challenge is re-written more concisely in the code editor. Use store.getState() to retrieve the state from the store, and assign this to a new variable currentState.
