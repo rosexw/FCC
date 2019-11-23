47eb569785e3549f2823b81899a3894ce40387bd
rosexw <rose.xw.wan@gmail.com>
2019-11-22 19:33:54 -0500

    renamed all the react exercises to contain exercise number

--- react_exercises/React-27-simple-counter.js
+++ react_exercises/React-27-simple-counter.js
@@ -46,7 +46,7 @@ class Counter extends React.Component {
   
 
 
-// React: Write a Simple Counter
+// React: Write a Simple Counter 27/47
 // You can design a more complex stateful component by combining the concepts covered so far. These include initializing state, writing methods that set state, and assigning click handlers to trigger these methods.
 
 // The Counter component keeps track of a count value in state. There are two buttons which call methods increment() and decrement(). Write these methods so the counter value is incremented or decremented by 1 when the appropriate button is clicked. Also, create a reset() method so when the reset button is clicked, the count is set to 0.
